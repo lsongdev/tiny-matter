1103316b89c63e1b637f0ad2a4052e24ad4031f2
jrson83 <me@jrson.de>
2022-03-10 20:42:08 +0100

    Fixed Carriage Return and Line Feed

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-const YAML = require('js-yaml');
+const YAML = require("js-yaml");
 
 /**
  * matter
- * @param {*} str 
- * @param {*} options 
- * @returns 
+ * @param {*} str
+ * @param {*} options
+ * @returns
  */
-const matter = (str = '', options) => {
-  const { delimiters, parser: parse } = Object.assign({
-    delimiters: ['---', '---'],
-    parser: YAML.load,
-  }, options);
+const matter = (str = "", options) => {
+  const { delimiters, parser: parse } = Object.assign(
+    {
+      delimiters: ["---", "---"],
+      parser: YAML.load,
+    },
+    options
+  );
   const [open, close] = delimiters;
-  const r1 = new RegExp(`^${open}\n`);
-  const r2 = new RegExp(`\n${close}\n`);
-  if (!(r1.test(str) && r2.test(str)))
-    return { content: str };
+  const r1 = new RegExp(`^${open}[\r\n]`);
+  const r2 = new RegExp(`[\r\n]${close}[\r\n]`);
+  if (!(r1.test(str) && r2.test(str))) return { content: str };
   const x = r1.exec(str);
   const i = x[0].length;
   const y = r2.exec(str.substr(i));
@@ -23,7 +25,13 @@ const matter = (str = '', options) => {
   const k = j + y[0].length;
   const front = str.substring(i, j);
   const data = parse(front);
-  const content = str.slice(k);
+  let content = str.slice(k);
+  if (content[0] === "\r") {
+    content = content.slice(1);
+  }
+  if (content[0] === "\n") {
+    content = content.slice(1);
+  }
   return { data, content };
 };
 
